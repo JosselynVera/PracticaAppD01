85a1e170317b7848a6ade83d43786807546fb27e
mchimbof <isaiaschimbof@gmail.com>
2023-12-01 12:14:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/practicad01/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.practicad01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import WebServices.Asynchtask;
+import WebServices.WebService;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+        TextView txtResp = (TextView) findViewById(R.id.txtVista);
+        String lstNombres="";
+        JSONArray JSONlista = new JSONArray(result);
+        for(int i=0; i< JSONlista.length();i++){
+            JSONObject banco= JSONlista.getJSONObject(i);
+            lstNombres = lstNombres + "\n" +
+                    banco.getString("id") + " - " +
+                    banco.getString("name") + " - " +
+                    banco.getString("username").toString();
+        }
+        txtResp.setText("Respuesta WS Lista de Nombres" + lstNombres);
+    }
+
+    public void Bt(View view){
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new
+                WebService("https://jsonplaceholder.typicode.com/users",
+                datos, MainActivity.this, MainActivity.this);
+        ws.execute("GET");
+    }
+}
